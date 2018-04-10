698f134dc30856517efe5b5abce34b4d2062a9b8
Robert Neumayer <robert.neumayer@oracle.com>
2018-04-10 13:29:34 +0200

    Update versioning logic

---
+++ scripts/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source 8.0/VERSION
+
+REPO_ARG="ARG REPO=https://repo.mysql.com"
+MYSQL_SERVER_PACKAGE_URL_ARG="ARG MYSQL_SERVER_PACKAGE_URL=\$REPO/yum/mysql-8.0-community/docker/x86_64/mysql-community-server-minimal-$MYSQL_SERVER_RPM_VERSION.el7.x86_64.rpm"
+MYSQL_ROUTER_PACKAGE_URL_ARG="ARG MYSQL_ROUTER_PACKAGE_URL=\$REPO/yum/mysql-tools-preview/el/7/x86_64/mysql-router-$MYSQL_ROUTER_RPM_VERSION.el7.x86_64.rpm"
+
+sed -i -e "s#ARG REPO.*#$REPO_ARG#" 8.0/Dockerfile
+
+sed -i -e "s#ARG MYSQL_SERVER_PACKAGE_URL.*#$MYSQL_SERVER_PACKAGE_URL_ARG#" 8.0/Dockerfile
+sed -i -e "s#ARG MYSQL_ROUTER_PACKAGE_URL.*#$MYSQL_ROUTER_PACKAGE_URL_ARG#" 8.0/Dockerfile
+sed -i -e "s/with_version.*/with_version('$MYSQL_ROUTER_PACKAGE_VERSION')/" 8.0/spec/Dockerfile_spec.rb
