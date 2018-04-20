94a193aacfdfc3238980d84e940caa75dc80fec0
Robert Neumayer <robert.neumayer@oracle.com>
2018-04-20 15:13:46 +0200

    Bump version to 8.0.11

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@ source 8.0/VERSION
 
 REPO_ARG="ARG REPO=https://repo.mysql.com"
 MYSQL_SERVER_PACKAGE_URL_ARG="ARG MYSQL_SERVER_PACKAGE_URL=\$REPO/yum/mysql-8.0-community/docker/x86_64/mysql-community-server-minimal-$MYSQL_SERVER_RPM_VERSION.el7.x86_64.rpm"
-MYSQL_ROUTER_PACKAGE_URL_ARG="ARG MYSQL_ROUTER_PACKAGE_URL=\$REPO/yum/mysql-tools-preview/el/7/x86_64/mysql-router-$MYSQL_ROUTER_RPM_VERSION.el7.x86_64.rpm"
+MYSQL_ROUTER_PACKAGE_URL_ARG="ARG MYSQL_ROUTER_PACKAGE_URL=\$REPO/yum/mysql-tools-community/el/7/x86_64/mysql-router-$MYSQL_ROUTER_RPM_VERSION.el7.x86_64.rpm"
 
 sed -i -e "s#ARG REPO.*#$REPO_ARG#" 8.0/Dockerfile
 
