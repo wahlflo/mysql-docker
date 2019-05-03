c79802712bfe53e2c0195a0182268169dfcbcc2a
Robert Neumayer <robert.neumayer@oracle.com>
2019-05-03 12:08:08 +0200

    Bump version to 8.0.16
    Change-Id: I9fe3d1ca93f68858fdaabac1a9d20fccac1a5ab0

--- 8.0/inspec/control.rb
+++ 8.0/inspec/control.rb
@@ -12,10 +12,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '8.0.15.*' }
+    its ('version') { should match '8.0.16.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.0.15.*' }
+    its ('version') { should match '8.0.16.*' }
   end
 end
