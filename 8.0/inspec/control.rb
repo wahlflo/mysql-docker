a411b4d5a85c3dcbb1dd83bea0e16517200c0502
MySQL Build Team <mysql-build@oss.oracle.com>
2019-10-23 12:42:23 +0000

    Release version 0.8-router
      * Bump version to 8.0.18

--- 8.0/inspec/control.rb
+++ 8.0/inspec/control.rb
@@ -12,10 +12,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '8.0.16.*' }
+    its ('version') { should match '8.0.18.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.0.16.*' }
+    its ('version') { should match '8.0.18.*' }
   end
 end
