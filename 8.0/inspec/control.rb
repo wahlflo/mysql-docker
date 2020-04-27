329cee55d1a8651b2c789c900419ed00eb8ae2b7
MySQL Build Team <mysql-build@oss.oracle.com>
2020-04-27 05:07:34 +0000

    Release version 0.10-router
      * Bumped versions for release

--- 8.0/inspec/control.rb
+++ 8.0/inspec/control.rb
@@ -12,10 +12,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '8.0.19.*' }
+    its ('version') { should match '8.0.20.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.0.19.*' }
+    its ('version') { should match '8.0.20.*' }
   end
 end
